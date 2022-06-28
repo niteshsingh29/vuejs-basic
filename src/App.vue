a2a38623f48f0bf2b5b11f828085a8d6476bb159
Nitesh Singh <92999235+niteshsingh29@users.noreply.github.com>
2022-06-29 00:47:23 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <parentComponent />
+</template>
+
+<script>
+import parentComponent from "./components/parentcomponent.vue";
+export default {
+  name: "App",
+  components: {
+    parentComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/childcomponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>{{ advice }}</div>
+</template>
+
+<script>
+export default {
+  name: "childcomponent",
+  props: {
+    advice: String,
+  },
+};
+</script>
